60047543f350a8f56f642e681c58cec1a10f226d
IrushaChamalka <135311090+IrushaChamalka@users.noreply.github.com>
2024-10-28 22:14:19 +0530

    login form validation

--- signup/signup.php
+++ signup/signup.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root"; // Database username
 $password = ""; // Database password
-$dbname = "your_database_name"; // Your database name
+$dbname = "mc"; // Your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,8 +13,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$year = mysqli_real_escape_string($conn,$_POST['year']);
+$department = mysqli_real_escape_string($conn,$_POST['dpt']);
+$reg_no = mysqli_real_escape_string($conn,$_POST['reg_no']);
+
 // Retrieve form data
-$reg_number = mysqli_real_escape_string($conn, $_POST['reg_number']);
+$reg_number = $year.$department.$reg_no;
 $full_name = mysqli_real_escape_string($conn, $_POST['full_name']);
 $name_with_initials = mysqli_real_escape_string($conn, $_POST['name_with_initials']);
 $gender = mysqli_real_escape_string($conn, $_POST['gender']);
@@ -32,13 +36,14 @@ $weight = mysqli_real_escape_string($conn, $_POST['weight']);
 $gardian_name = mysqli_real_escape_string($conn, $_POST['gardian_name']);
 $relation = mysqli_real_escape_string($conn, $_POST['relation']);
 $contact_number = mysqli_real_escape_string($conn, $_POST['contact_number']);
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hashing password for security
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$re_password = password_hash($_POST['re_password'], PASSWORD_DEFAULT); // Hashing password for security
 $blood_group = mysqli_real_escape_string($conn, $_POST['blood_group']);
 $last_school_attend = mysqli_real_escape_string($conn, $_POST['last_school_attend']);
 $profile_photo = mysqli_real_escape_string($conn, $_POST['profile_photo']);
 
 // SQL query to insert the user into the database
-$sql = "INSERT INTO user_details (reg_number, full_name, name_with_initials, gender, dob, mobile_number, email, residential_address, permenent_address, nic, martial_status, faculty, department, height, weight, gardian_name, relation, contact_number, password, blood_group, last_school_attend, profile_photo)
+$sql = "INSERT INTO user_details (reg_number, full_name, name_with_initials, gender, dob, mobile_number, email, residential_address, permenent_address, nic, martial_status, faculty, department, height, weight, gardian_name, relation, contact_number, password , blood_group, last_school_attend, profile_photo)
 VALUES ('$reg_number', '$full_name', '$name_with_initials', '$gender', '$dob', '$mobile_number', '$email', '$residential_address', '$permenent_address', '$nic', '$martial_status', '$faculty', '$department', '$height', '$weight', '$gardian_name', '$relation', '$contact_number', '$password', '$blood_group', '$last_school_attend', '$profile_photo')";
 
 if ($conn->query($sql) === TRUE) {
