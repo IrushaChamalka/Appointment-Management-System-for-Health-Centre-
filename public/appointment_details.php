3922dd0bae0ea38abaaa91aaf29d160ada8d7bc7
Savidya Perera <snirthana5@gmail.com>
2024-11-07 17:04:07 +0530

    C11-v01

--- public/appointment_details.php
+++ public/appointment_details.php
@@ -31,7 +31,7 @@
         <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Additional Information</h5>
         </div>
-        <form action="" method="post">
+        <form action="submit_appointment.php" method="post">
         <div class="modal-body">
             <div class="form-group">
                 <label for="">REMARK:</label>
@@ -48,22 +48,3 @@
         </form>
     </div>
 </div>
-<?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        var_dump($_POST);
-        if(isset($_POST["appoinment_submit"])){
-            $reg = $row['reg_number'];
-            $name = $row['name_with_initials'];
-            $email = $row['email'];
-            $gender = $row['gender'];
-            $remark = $_POST['remark'];
-            $push_apt_sql = "INSERT INTO `bookings`(`number`, `reg_number`, `name`, `email`, `date`, `remark`, `gender`) VALUES ('$sheet','$reg','$name','$email','$selected','$remark', '$gender')";
-            mysqli_query($conn, $push_apt_sql);
-            ?>
-                <script>window.location.href = "./"</script>
-            <?php
-        }
-        
-    }
-    
-?>
